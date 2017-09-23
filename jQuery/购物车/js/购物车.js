c3e832dd7064d72a8994432806c5225c14d290ef
xiocngchunyang <2397707633@qq.com>
2017-09-23 23:06:14 +0800

    abc
    abc

--- jQuery/购物车/js/购物车.js
+++ jQuery/购物车/js/购物车.js
@@ -2,6 +2,11 @@
  * Created by Administrator on 2017\9\11 0011.
  */
 $(function(){
+    //全部商品数量
+    var allnum=$(".every_check").length;
+    $(".head").find("span").html(allnum);
+
+
     //点击全选
     $(".all").click(function(){
         if($(this).prop("checked")){
@@ -11,6 +16,8 @@ $(function(){
             $(".goods_every").removeClass("goods_checked").find("input[type=checkbox]").prop("checked",false);
             $(".store1_title").find("input[type=checkbox]").prop("checked",false);
         }
+        numcheck();
+        price4();
     });
 
     //点击店铺选择
@@ -27,12 +34,18 @@ $(function(){
             $(this).parents(".store1").find(".goods_every").removeClass("goods_checked").find("input").prop("checked",false);
             $(".all").prop("checked",false);
         }
+        price3($(this));
+        numcheck();
+        price4()
     });
+
+
     //点击每一项选择
     $(".every_check").click(function () {
         var a=$(this).parents(".store1").find(".every_check");//店铺商品数
         var b=$(this).parents(".store1").find(".every_check:checked");//店铺内被选中的商品数
-        var c=$(this).parents(".store1").find(".store1_check");//店铺全选按钮
+        var c=$(this).parents(".store1").find(".store1_check");
+        //店铺全选按钮
          if ($(this).prop("checked")){
         $(this).parents(".goods_every").addClass("goods_checked");
     }else{
@@ -49,7 +62,8 @@ $(function(){
             c.prop("checked",false)
         }
         price2($(this));
-
+        numcheck();
+        price4()
     });
     //右键点击
 
@@ -60,8 +74,13 @@ $(function(){
             nums ++;
         }
         $(this).siblings(".nums").val(parseInt(nums));
-        price1($(this))
+        price1($(this));
+        if ($(this).parents(".goods_every").find(".every_check").prop("checked")){
+        price2($(this).parents(".goods_every").find(".every_check"))
+        }
+        price4()
     });
+
     //左键点击
     $(".plus").click(function () {
         var nums=$(this).siblings(".nums").val();
@@ -73,32 +92,81 @@ $(function(){
             nums --;
         }
         $(this).siblings(".nums").val(nums);
-        price1($(this))
+        price1($(this));
+        if ($(this).parents(".goods_every").find(".every_check").prop("checked")){
+            price2($(this).parents(".goods_every").find(".every_check"))
+        }
+        price4()
     });
+
+
     //商品价格
     function price1(btn) {
         var unit_price=btn.parents(".goods_every").find(".goods_price a").html();
         var nums=btn.siblings(".nums").val();
         var price1=btn.parents(".store1_goods_cont").find(".unit_price");
-        console.log(nums);
         price1.html((unit_price*nums).toFixed(2))
     }
     //店铺商品价格
     function price2(d) {
-        var sum=0;
-        d.parents(".store1_goods").find(".every_check").each(function () {
-            if ($(this).prop("checked")){
-                 var price= $(this).parents(".goods_every").find(".unit_price").html();
-                        //.css("background","red");
-                 console.log(price);
-                // sum=sum+price;
+        if (d.prop("checked")){
+            var sum=0;
+            d.parents(".store1_goods").find(".every_check").each(function () {
+                if ($(this).prop("checked")){
+                    var price= $(this).parents(".goods_every").find(".unit_price").html();
+                    sum=(parseFloat(sum)+parseFloat(price)).toFixed(2);
 
-            }
-            // $(this).parents(".store1_cont").find(".subtotal").html(sum)
+                }
+                $(this).parents(".store1_cont").find(".subtotal").html(sum)
 
-        })
+            })
+        }else {
+            d.parents(".store1").find(".subtotal").html("")
+        }
+
+    }
+//点击店铺全选时
+    function price3(c) {
+        if(c.prop("checked")){
+            var sum=0;
+            c.parents(".store1").find(".every_check").each(function () {
+                if($(this).prop("checked")){
+                    var price= $(this).parents(".goods_every").find(".unit_price").html();
+                    sum=(parseFloat(sum)+parseFloat(price)).toFixed(2);
+                }
+                $(this).parents(".store1").find(".subtotal").html(sum)
+            });
+        } else {
+            c.parents(".store1").find(".subtotal").html("")
+        }
     }
 
+//选中商品数量
+    function numcheck() {
+        var num=0;
+        $(".every_check").each(function () {
+            if($(this).prop("checked")){
+                num++
+            }
+        });
+        $(".all_nums").find("span").html(num);
+    }
+
+//所有选中商品价格
+    function price4(e) {
+        var num=0;
+        $(".subtotal").each(function () {
+            if($(this).html()==""){
+                i=0;
+            }else {
+                i=parseFloat($(this).html())
+                console.log(i)
+            }
+            num=num+i;
+        });
+        $(".all_price").find("span").html(num.toFixed(2));
+
+    }
 
 
 
